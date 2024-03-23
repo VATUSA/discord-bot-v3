70c985cda198a9fa336508608696ae8f0fe43a34
Jon <jonhasacat8@gmail.com>
2024-03-23 10:33:25 -0700

    Disable interval refresh to address API volume issue

--- internal/bot/client.go
+++ internal/bot/client.go
@@ -29,7 +29,7 @@ func Run() {
 
 	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
 		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
-		go IntervalRefreshAll(s)
+		//go IntervalRefreshAll(s)
 		go IntervalReloadConfigs()
 		go func() {
 			for {
